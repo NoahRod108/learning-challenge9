8a6adbb895f7cecf963d90ae2cafcf199fccd733
Noah Rodriguez <nrodrig3@valpo.edu>
2023-03-06 15:25:42 -0600

    added mobile style

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // navbar
 const navbar = document.querySelector(".navbar");
 const toggleNav = document.querySelector(".mobile-menu-toggle");
+const overlay = document.querySelector(".overlay");
 
 toggleNav.addEventListener('click', () => {
     const visible = navbar.getAttribute("data-visible");
     toggleNav.setAttribute("aria-expanded", "false")
 
     navbar.setAttribute("data-visible", "false");
+    overlay.setAttribute("data-visible", "false");
 
     if(visible === "false"){
         navbar.setAttribute("data-visible", "true");
+        overlay.setAttribute("data-visible", "true");
         toggleNav.setAttribute("aria-expanded", "true");
     }
 })
